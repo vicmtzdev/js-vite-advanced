07bb2564b425e3a079ae4639fcdce5fcb5c1c902
Victor Martinez <vicmtzdev@gmail.com>
2023-06-14 00:24:11 -0600

    04-promise-race

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { environmentsComponent } from './src/concepts/01-environments';
 import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
+import { promiseRaceComponent } from './src/concepts/04-promise-race';
 
 
 document.querySelector('#app').innerHTML = `
@@ -26,5 +27,6 @@ const element = document.querySelector('.card');
 
 // environmentsComponent(element);
 // callbacksComponent(element);
-promiseComponent(element);
+// promiseComponent(element);
+promiseRaceComponent(element);
 
---
+++ src/concepts/04-promise-race.js
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const promiseRaceComponent = (element) => {
+
+    element.innerHTML = 'Loading...';
+
+    const renderValue = (value) => {
+
+        element.innerHTML = value;
+
+    }
+
+    Promise.race([
+        slowPromise(),
+        fastPromise(),
+        mediumPromise(),
+    ]).then(renderValue);
+
+}
+
+const slowPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Slow Promise');
+    }, 2000);
+})
+
+const mediumPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Medium Promise');
+    }, 1500);
+})
+
+const fastPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Fast Promise');
+    }, 1000);
+})
