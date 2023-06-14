9af33c61b6e438e36f55261af197648bbc858503
Victor Martinez <vicmtzdev@gmail.com>
2023-06-14 12:15:59 -0600

    06-async-await

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
 import { asyncComponent } from './src/concepts/05-async';
+import { asyncAwaitComponent } from './src/concepts/06-async-await';
 
 
 document.querySelector('#app').innerHTML = `
@@ -30,5 +31,6 @@ const element = document.querySelector('.card');
 // callbacksComponent(element);
 // promiseComponent(element);
 // promiseRaceComponent(element);
-asyncComponent(element);
+// asyncComponent(element);
+asyncAwaitComponent(element);
 
