7f8094dd9af1063b5f96b0843cb0d61e135c7138
Victor Martinez <vicmtzdev@gmail.com>
2023-06-14 11:48:25 -0600

    05-async

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { environmentsComponent } from './src/concepts/01-environments';
 import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
+import { asyncComponent } from './src/concepts/05-async';
 
 
 document.querySelector('#app').innerHTML = `
@@ -28,5 +29,6 @@ const element = document.querySelector('.card');
 // environmentsComponent(element);
 // callbacksComponent(element);
 // promiseComponent(element);
-promiseRaceComponent(element);
+// promiseRaceComponent(element);
+asyncComponent(element);
 
---
+++ src/concepts/05-async.js
@@ -0,0 +1,32 @@
+import { heroes } from "../data/heroes"
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = (element) => {
+
+    const id1 = '5d86371fd55e2e2a30fe1ccb';
+    console.log('Inicio del componente');
+
+    findHero(id1)
+        .then(name => element.innerHTML = name)
+        .catch(error => element.innerHTML = error)
+
+    console.log('Fin del componente');
+}
+
+
+
+/**
+ * 
+ * @param {String} id
+ * @returns {Promise<String>} name 
+ */
+const findHero = async (id) => {
+
+    const hero = heroes.find(hero => hero.id === id);
+    if (!hero) throw `Hero with id: ${id} not found`;
+    return hero.name;
+
+}
