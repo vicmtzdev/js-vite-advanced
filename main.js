d08bc169826ddb52a1a0068e4fed51610759f4c6
Victor Martinez <vicmtzdev@gmail.com>
2023-06-15 12:13:28 -0600

    10-generators-async

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
 import { forAwaitComponent } from './src/concepts/08-for-await';
 import { generatorFunctionsComponent } from './src/concepts/09-generators';
+import { generatorsAsyncComponent } from './src/concepts/10-generators-async';
 
 
 document.querySelector('#app').innerHTML = `
@@ -38,6 +39,7 @@ const element = document.querySelector('.card');
 // asyncAwaitComponent(element);
 // asyncAwait2Component(element);
 // forAwaitComponent(element);
-generatorFunctionsComponent(element);
+// generatorFunctionsComponent(element);
+generatorsAsyncComponent(element);
 
 
---
+++ src/concepts/10-generators-async.js
@@ -0,0 +1,50 @@
+import { heroes } from '../data/heroes';
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const generatorsAsyncComponent = async (element) => {
+
+    const heroGenerator = getHeroGenerator();
+    let isFinished = false;
+
+    do {
+
+        const { value, done } = await heroGenerator.next();
+        isFinished = done;
+
+        if (isFinished) break;
+
+        element.innerHTML = value;
+
+    } while (!isFinished);
+
+}
+
+
+async function* getHeroGenerator() {
+
+    for (const hero of heroes) {
+        await sleep();
+        yield hero.name;
+    }
+
+    return 'No hay más';
+
+}
+
+const sleep = () => {
+
+    return new Promise((resolve) => {
+
+        setTimeout(() => {
+
+            resolve();
+
+        }, 1000);
+
+    })
+
+}
